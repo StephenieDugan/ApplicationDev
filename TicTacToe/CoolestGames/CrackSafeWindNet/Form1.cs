1bd94ba4203d53996d337da1e03c69aa937d6b86
StephenieDugan <stepheniedugan617@gmail.com>
2023-10-26 10:21:01 -0600

    saaaaaaaaaaave

---
+++ TicTacToe/CoolestGames/CrackSafeWindNet/Form1.cs
@@ -0,0 +1,22 @@
+namespace CrackSafeWindNet;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+        checkBox1.Text = "cool";
+
+        checkBox1.CheckedChanged += ColorChanger;
+    }
+
+    public void ColorChanger(Object sender, EventArgs e)
+    {
+        checkBox1.ForeColor = Color.DarkRed;
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+}
