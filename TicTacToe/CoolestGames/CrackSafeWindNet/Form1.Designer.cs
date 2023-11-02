075ad2cd6f4142acdb71e8681ff03febaeef3167
StephenieDugan <stepheniedugan617@gmail.com>
2023-11-01 18:24:43 -0600

    save and done

--- TicTacToe/CoolestGames/CrackSafeWindNet/Form1.Designer.cs
+++ TicTacToe/CoolestGames/CrackSafeWindNet/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace CrackSafeWindNet;
+﻿using System.Windows.Forms;
+
+namespace CrackSafeWindNet;
 
 partial class Form1
 {
@@ -28,99 +30,34 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        checkBox1 = new CheckBox();
-        groupBox1 = new GroupBox();
-        radioButton1 = new RadioButton();
-        radioButton2 = new RadioButton();
-        radioButton3 = new RadioButton();
         btnStart = new Button();
         tableLayoutPanel1 = new TableLayoutPanel();
-        pictureBox5 = new PictureBox();
-        pictureBox4 = new PictureBox();
-        pictureBox3 = new PictureBox();
-        pictureBox2 = new PictureBox();
+        btnExit = new Button();
         label1 = new Label();
         label2 = new Label();
         label3 = new Label();
-        label4 = new Label();
-        label5 = new Label();
-        pictureBox1 = new PictureBox();
+        pictureBoxCorrect = new PictureBox();
+        pictureBoxFail = new PictureBox();
+        pictureBoxClose = new PictureBox();
         txtGuess = new TextBox();
-        label6 = new Label();
-        groupBox1.SuspendLayout();
+        txtGuess1 = new TextBox();
+        txtGuess2 = new TextBox();
+        txtGuess3 = new TextBox();
+        btnGuess = new Button();
+        btnCheat = new Button();
+        label4 = new Label();
         tableLayoutPanel1.SuspendLayout();
-        ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
-        ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
-        ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-        ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-        ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+        ((System.ComponentModel.ISupportInitialize)pictureBoxCorrect).BeginInit();
+        ((System.ComponentModel.ISupportInitialize)pictureBoxFail).BeginInit();
+        ((System.ComponentModel.ISupportInitialize)pictureBoxClose).BeginInit();
         SuspendLayout();
-        // 
-        // checkBox1
-        // 
-        checkBox1.AutoSize = true;
-        checkBox1.Location = new Point(23, 21);
-        checkBox1.Margin = new Padding(2);
-        checkBox1.Name = "checkBox1";
-        checkBox1.Size = new Size(49, 19);
-        checkBox1.TabIndex = 0;
-        checkBox1.Text = "cool";
-        checkBox1.UseVisualStyleBackColor = true;
-        checkBox1.CheckedChanged += checkBox1_CheckedChanged;
-        // 
-        // groupBox1
-        // 
-        groupBox1.Controls.Add(radioButton1);
-        groupBox1.Controls.Add(radioButton2);
-        groupBox1.Controls.Add(radioButton3);
-        groupBox1.Location = new Point(23, 45);
-        groupBox1.Name = "groupBox1";
-        groupBox1.Size = new Size(119, 100);
-        groupBox1.TabIndex = 3;
-        groupBox1.TabStop = false;
-        groupBox1.Text = "groupBox1";
-        // 
-        // radioButton1
-        // 
-        radioButton1.AutoSize = true;
-        radioButton1.Location = new Point(6, 24);
-        radioButton1.Name = "radioButton1";
-        radioButton1.Size = new Size(48, 19);
-        radioButton1.TabIndex = 4;
-        radioButton1.TabStop = true;
-        radioButton1.Text = "Easy";
-        radioButton1.UseVisualStyleBackColor = true;
-        radioButton1.CheckedChanged += radioButton1_CheckedChanged;
-        // 
-        // radioButton2
-        // 
-        radioButton2.AutoSize = true;
-        radioButton2.Location = new Point(5, 49);
-        radioButton2.Name = "radioButton2";
-        radioButton2.Size = new Size(70, 19);
-        radioButton2.TabIndex = 5;
-        radioButton2.TabStop = true;
-        radioButton2.Text = "Medium";
-        radioButton2.UseVisualStyleBackColor = true;
-        radioButton2.CheckedChanged += radioButton2_CheckedChanged;
-        // 
-        // radioButton3
-        // 
-        radioButton3.AutoSize = true;
-        radioButton3.Location = new Point(5, 74);
-        radioButton3.Name = "radioButton3";
-        radioButton3.Size = new Size(51, 19);
-        radioButton3.TabIndex = 0;
-        radioButton3.TabStop = true;
-        radioButton3.Text = "Hard";
-        radioButton3.UseVisualStyleBackColor = true;
-        radioButton3.CheckedChanged += radioButton3_CheckedChanged;
         // 
         // btnStart
         // 
-        btnStart.Location = new Point(12, 225);
+        btnStart.Location = new Point(12, 390);
+        btnStart.Margin = new Padding(4, 5, 4, 5);
         btnStart.Name = "btnStart";
-        btnStart.Size = new Size(444, 25);
+        btnStart.Size = new Size(312, 42);
         btnStart.TabIndex = 4;
         btnStart.Text = "Start";
         btnStart.UseVisualStyleBackColor = true;
@@ -129,186 +66,221 @@ partial class Form1
         // tableLayoutPanel1
         // 
         tableLayoutPanel1.ColumnCount = 2;
-        tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
-        tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 75F));
-        tableLayoutPanel1.Controls.Add(pictureBox5, 0, 4);
-        tableLayoutPanel1.Controls.Add(pictureBox4, 0, 3);
-        tableLayoutPanel1.Controls.Add(pictureBox3, 0, 2);
-        tableLayoutPanel1.Controls.Add(pictureBox2, 0, 1);
+        tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 72.3756943F));
+        tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 27.6243114F));
+        tableLayoutPanel1.Controls.Add(btnExit, 0, 0);
         tableLayoutPanel1.Controls.Add(label1, 1, 0);
         tableLayoutPanel1.Controls.Add(label2, 1, 1);
         tableLayoutPanel1.Controls.Add(label3, 1, 2);
-        tableLayoutPanel1.Controls.Add(label4, 1, 3);
-        tableLayoutPanel1.Controls.Add(label5, 1, 4);
-        tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
-        tableLayoutPanel1.Location = new Point(372, 0);
+        tableLayoutPanel1.Location = new Point(684, 0);
+        tableLayoutPanel1.Margin = new Padding(4, 5, 4, 5);
         tableLayoutPanel1.Name = "tableLayoutPanel1";
-        tableLayoutPanel1.RowCount = 5;
-        tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-        tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-        tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-        tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-        tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-        tableLayoutPanel1.Size = new Size(98, 109);
+        tableLayoutPanel1.RowCount = 3;
+        tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 16.666666F));
+        tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 16.666666F));
+        tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 16.666666F));
+        tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 16.666666F));
+        tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 16.666666F));
+        tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 16.666666F));
+        tableLayoutPanel1.Size = new Size(469, 367);
         tableLayoutPanel1.TabIndex = 5;
+        Controls.Add(tableLayoutPanel1);
         // 
-        // pictureBox5
+        // btnExit
         // 
-        pictureBox5.Location = new Point(3, 91);
-        pictureBox5.Name = "pictureBox5";
-        pictureBox5.Size = new Size(18, 15);
-        pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
-        pictureBox5.TabIndex = 15;
-        pictureBox5.TabStop = false;
-        // 
-        // pictureBox4
-        // 
-        pictureBox4.Location = new Point(3, 71);
-        pictureBox4.Name = "pictureBox4";
-        pictureBox4.Size = new Size(18, 14);
-        pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
-        pictureBox4.TabIndex = 14;
-        pictureBox4.TabStop = false;
-        // 
-        // pictureBox3
-        // 
-        pictureBox3.Location = new Point(3, 51);
-        pictureBox3.Name = "pictureBox3";
-        pictureBox3.Size = new Size(18, 14);
-        pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
-        pictureBox3.TabIndex = 13;
-        pictureBox3.TabStop = false;
-        // 
-        // pictureBox2
-        // 
-        pictureBox2.Location = new Point(3, 27);
-        pictureBox2.Name = "pictureBox2";
-        pictureBox2.Size = new Size(18, 18);
-        pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
-        pictureBox2.TabIndex = 12;
-        pictureBox2.TabStop = false;
+        btnExit.Location = new Point(4, 5);
+        btnExit.Margin = new Padding(4, 5, 4, 5);
+        btnExit.Name = "btnExit";
+        btnExit.Size = new Size(200, 43);
+        btnExit.TabIndex = 15;
+        btnExit.Text = "Exit";
+        btnExit.UseVisualStyleBackColor = true;
+        btnExit.Click += btnExit_Click;
         // 
         // label1
         // 
         label1.Anchor = AnchorStyles.None;
-        label1.Location = new Point(27, 0);
+        label1.Location = new Point(362, 42);
+        label1.Margin = new Padding(4, 0, 4, 0);
         label1.Name = "label1";
-        label1.Size = new Size(68, 23);
+        label1.Size = new Size(84, 38);
         label1.TabIndex = 6;
         label1.Text = "label1";
         // 
         // label2
         // 
         label2.Anchor = AnchorStyles.None;
-        label2.Location = new Point(27, 24);
+        label2.Location = new Point(362, 164);
+        label2.Margin = new Padding(4, 0, 4, 0);
         label2.Name = "label2";
-        label2.Size = new Size(68, 23);
+        label2.Size = new Size(84, 38);
         label2.TabIndex = 7;
         label2.Text = "label2";
         // 
         // label3
         // 
         label3.Anchor = AnchorStyles.None;
-        label3.Location = new Point(27, 48);
+        label3.Location = new Point(362, 289);
+        label3.Margin = new Padding(4, 0, 4, 0);
         label3.Name = "label3";
-        label3.Size = new Size(68, 20);
+        label3.Size = new Size(84, 33);
         label3.TabIndex = 8;
         label3.Text = "label3";
         // 
-        // label4
+        // pictureBoxCorrect
         // 
-        label4.Anchor = AnchorStyles.None;
-        label4.Location = new Point(27, 68);
-        label4.Name = "label4";
-        label4.Size = new Size(68, 20);
-        label4.TabIndex = 9;
-        label4.Text = "label4";
+        pictureBoxCorrect.Location = new Point(368, 14);
+        pictureBoxCorrect.Margin = new Padding(4, 5, 4, 5);
+        pictureBoxCorrect.Name = "pictureBoxCorrect";
+        pictureBoxCorrect.Size = new Size(232, 146);
+        pictureBoxCorrect.SizeMode = PictureBoxSizeMode.StretchImage;
+        pictureBoxCorrect.TabIndex = 11;
+        pictureBoxCorrect.TabStop = false;
         // 
-        // label5
+        // pictureBoxFail
         // 
-        label5.Anchor = AnchorStyles.None;
-        label5.Location = new Point(27, 88);
-        label5.Name = "label5";
-        label5.Size = new Size(68, 21);
-        label5.TabIndex = 10;
-        label5.Text = "label5";
+        pictureBoxFail.Location = new Point(368, 14);
+        pictureBoxFail.Margin = new Padding(4, 5, 4, 5);
+        pictureBoxFail.Name = "pictureBoxFail";
+        pictureBoxFail.Size = new Size(232, 146);
+        pictureBoxFail.SizeMode = PictureBoxSizeMode.StretchImage;
+        pictureBoxFail.TabIndex = 13;
+        pictureBoxFail.TabStop = false;
         // 
-        // pictureBox1
+        // pictureBoxClose
         // 
-        pictureBox1.Location = new Point(3, 3);
-        pictureBox1.Name = "pictureBox1";
-        pictureBox1.Size = new Size(18, 18);
-        pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-        pictureBox1.TabIndex = 11;
-        pictureBox1.TabStop = false;
+        pictureBoxClose.Location = new Point(368, 14);
+        pictureBoxClose.Margin = new Padding(4, 5, 4, 5);
+        pictureBoxClose.Name = "pictureBoxClose";
+        pictureBoxClose.Size = new Size(232, 146);
+        pictureBoxClose.SizeMode = PictureBoxSizeMode.StretchImage;
+        pictureBoxClose.TabIndex = 12;
+        pictureBoxClose.TabStop = false;
         // 
         // txtGuess
         // 
         txtGuess.Anchor = AnchorStyles.Right;
         txtGuess.Font = new Font("Segoe UI", 48F, FontStyle.Regular, GraphicsUnit.Point);
-        txtGuess.Location = new Point(187, 119);
+        txtGuess.Location = new Point(267, 198);
+        txtGuess.Margin = new Padding(4, 5, 4, 5);
         txtGuess.Name = "txtGuess";
-        txtGuess.Size = new Size(100, 93);
+        txtGuess.Size = new Size(141, 135);
         txtGuess.TabIndex = 6;
         txtGuess.Text = "00";
         txtGuess.TextAlign = HorizontalAlignment.Center;
         // 
-        // label6
+        // txtGuess1
         // 
-        label6.Anchor = AnchorStyles.None;
-        label6.Location = new Point(205, 85);
-        label6.Name = "label6";
-        label6.Size = new Size(68, 21);
-        label6.TabIndex = 11;
-        label6.Text = "label6";
+        txtGuess1.Anchor = AnchorStyles.Right;
+        txtGuess1.Font = new Font("Segoe UI", 48F, FontStyle.Regular, GraphicsUnit.Point);
+        txtGuess1.Location = new Point(39, 170);
+        txtGuess1.Margin = new Padding(4, 5, 4, 5);
+        txtGuess1.Name = "txtGuess1";
+        txtGuess1.Size = new Size(177, 135);
+        txtGuess1.TabIndex = 6;
+        txtGuess1.Text = "00";
+        txtGuess1.TextAlign = HorizontalAlignment.Center;
+        // 
+        // txtGuess2
+        // 
+        txtGuess2.Anchor = AnchorStyles.Right;
+        txtGuess2.Font = new Font("Segoe UI", 48F, FontStyle.Regular, GraphicsUnit.Point);
+        txtGuess2.Location = new Point(249, 170);
+        txtGuess2.Margin = new Padding(4, 5, 4, 5);
+        txtGuess2.Name = "txtGuess2";
+        txtGuess2.Size = new Size(177, 135);
+        txtGuess2.TabIndex = 7;
+        txtGuess2.Text = "00";
+        txtGuess2.TextAlign = HorizontalAlignment.Center;
+        // 
+        // txtGuess3
+        // 
+        txtGuess3.Anchor = AnchorStyles.Right;
+        txtGuess3.Font = new Font("Segoe UI", 48F, FontStyle.Regular, GraphicsUnit.Point);
+        txtGuess3.Location = new Point(479, 170);
+        txtGuess3.Margin = new Padding(4, 5, 4, 5);
+        txtGuess3.Name = "txtGuess3";
+        txtGuess3.Size = new Size(177, 135);
+        txtGuess3.TabIndex = 8;
+        txtGuess3.Text = "00";
+        txtGuess3.TextAlign = HorizontalAlignment.Center;
+        // 
+        // btnGuess
+        // 
+        btnGuess.Location = new Point(332, 390);
+        btnGuess.Margin = new Padding(4, 5, 4, 5);
+        btnGuess.Name = "btnGuess";
+        btnGuess.Size = new Size(312, 42);
+        btnGuess.TabIndex = 9;
+        btnGuess.Text = "Guess";
+        btnGuess.UseVisualStyleBackColor = true;
+        btnGuess.Click += btnGuess_Click;
+        btnGuess.KeyPress += txtGuess_KeyPress;
+        // 
+        // btnCheat
+        // 
+        btnCheat.Location = new Point(652, 390);
+        btnCheat.Margin = new Padding(4, 5, 4, 5);
+        btnCheat.Name = "btnCheat";
+        btnCheat.Size = new Size(312, 42);
+        btnCheat.TabIndex = 10;
+        btnCheat.Text = "Cheat";
+        btnCheat.UseVisualStyleBackColor = true;
+        btnCheat.Click += btnCheat_Click;
+        // 
+        // label4
+        // 
+        label4.Anchor = AnchorStyles.None;
+        label4.Location = new Point(13, 67);
+        label4.Margin = new Padding(4, 0, 4, 0);
+        label4.Name = "label4";
+        label4.Size = new Size(347, 39);
+        label4.TabIndex = 14;
+        label4.Text = "label4";
         // 
         // Form1
         // 
-        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleDimensions = new SizeF(10F, 25F);
         AutoScaleMode = AutoScaleMode.Font;
         BackColor = SystemColors.Info;
-        ClientSize = new Size(468, 262);
-        Controls.Add(label6);
-        Controls.Add(txtGuess);
+        ClientSize = new Size(1153, 449);
+        Controls.Add(label4);
+        Controls.Add(btnCheat);
+        Controls.Add(pictureBoxFail);
+        Controls.Add(pictureBoxCorrect);
+        Controls.Add(pictureBoxClose);
+        Controls.Add(btnGuess);
+        Controls.Add(txtGuess3);
+        Controls.Add(txtGuess2);
+        Controls.Add(txtGuess1);
         Controls.Add(tableLayoutPanel1);
         Controls.Add(btnStart);
-        Controls.Add(groupBox1);
-        Controls.Add(checkBox1);
-        Margin = new Padding(2);
         Name = "Form1";
         Text = "Form1";
         Load += Form1_Load;
-        groupBox1.ResumeLayout(false);
-        groupBox1.PerformLayout();
         tableLayoutPanel1.ResumeLayout(false);
-        ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
-        ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
-        ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-        ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-        ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+        ((System.ComponentModel.ISupportInitialize)pictureBoxCorrect).EndInit();
+        ((System.ComponentModel.ISupportInitialize)pictureBoxFail).EndInit();
+        ((System.ComponentModel.ISupportInitialize)pictureBoxClose).EndInit();
         ResumeLayout(false);
         PerformLayout();
     }
 
     #endregion
-
-    private CheckBox checkBox1;
-    private Label label1;
-    private GroupBox groupBox1;
-    private RadioButton radioButton1;
-    private RadioButton radioButton2;
-    private RadioButton radioButton3;
     private Button btnStart;
     private TableLayoutPanel tableLayoutPanel1;
-    private Label label2;
-    private PictureBox pictureBox5;
-    private PictureBox pictureBox4;
-    private PictureBox pictureBox3;
-    private PictureBox pictureBox2;
-    private Label label3;
-    private Label label4;
-    private Label label5;
-    private PictureBox pictureBox1;
+    private PictureBox pictureBoxFail;
+    private PictureBox pictureBoxClose;
+    private PictureBox pictureBoxCorrect;
     private TextBox txtGuess;
-    private Label label6;
+    private TextBox txtGuess1;
+    private TextBox txtGuess2;
+    private TextBox txtGuess3;
+    private Button btnGuess;
+    private Button btnCheat;
+    private Label label4;
+    private Button btnExit;
+    private Label label1;
+    private Label label2;
+    private Label label3;
 }
--- TicTacToe/CoolestGames/CrackSafeWindNet/Form1.cs
+++ TicTacToe/CoolestGames/CrackSafeWindNet/Form1.cs
@@ -1,39 +1,30 @@
+using static System.Net.Mime.MediaTypeNames;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
 namespace CrackSafeWindNet;
 
 public partial class Form1 : Form
 {
-    Random random = new Random();
-    List<Label> lblResults = new List<Label>();
-    List<PictureBox> picResults = new List<PictureBox>();
-
-    int answer = 0;
-    int numGuess = 0;
-    int intRandomMax = 0;
+    private Random random = new Random();
+    private List<Label> lblResults = new List<Label>();
+    List<string> guessHistory = new List<string>();
+    private int[] safeNumbers = new int[3];
+    private int numTurn = 0;
+    private int maxTurns = 10;
     public Form1()
     {
         InitializeComponent();
+        pictureBoxCorrect.Image = Properties.Resources.open_lock;
+        pictureBoxClose.Image = Properties.Resources.Working_on_lock;
+        pictureBoxFail.Image = Properties.Resources.Locked_lock;
         InitializeGame();
         ResetGame();
 
-        //checkBox1.CheckedChanged += ColorChanger;
     }
-
-    public void ColorChanger(Object sender, EventArgs e)
-    {
-        checkBox1.ForeColor = Color.DarkRed;
-    }
-
     private void Form1_Load(object sender, EventArgs e)
-    {
-
-    }
-
-    private void checkBox1_CheckedChanged(object sender, EventArgs e)
-    {
-
-    }
-
-    private void label1_Click(object sender, EventArgs e)
     {
 
     }
@@ -44,59 +35,50 @@ public partial class Form1 : Form
         lblResults.Add(label2);
         lblResults.Add(label3);
         lblResults.Add(label4);
-        lblResults.Add(label5);
-        lblResults.Add(label6);
 
-        picResults.Add(pictureBox1);
-        picResults.Add(pictureBox2);
-        picResults.Add(pictureBox3);
-        picResults.Add(pictureBox4);
-        picResults.Add(pictureBox5);
+        pictureBoxCorrect.Visible = false;
+        pictureBoxClose.Visible = false;
+        pictureBoxFail.Visible = true;
+
+        safeNumbers[0] = random.Next(10);
+        safeNumbers[1] = random.Next(10);
+        safeNumbers[2] = random.Next(10);
     }
 
     private void ResetGame()
     {
-        foreach (Label labels in lblResults)
+        foreach (Label label in lblResults)
         {
-            labels.Text = "";
+            label.Text = "";
+            label.ForeColor = Color.Black;
         }
-
-        foreach (PictureBox pics in picResults)
-        {
-            pics.Image = null;
-        }
-
-        txtGuess.Enabled = false;
-        btnStart.Enabled = true;
+        pictureBoxCorrect.Visible = false;
+        pictureBoxClose.Visible = false;
+        pictureBoxFail.Visible = true;
+        guessHistory.Clear();
+        numTurn = 0;
+        UpdateLog("New game started.");
+        txtGuess1.Text = txtGuess2.Text = txtGuess3.Text = "";
+        btnCheat.Enabled = true;
         btnStart.Text = "Start";
-        numGuess = 0;
+        safeNumbers[0] = random.Next(10);
+        safeNumbers[1] = random.Next(10);
+        safeNumbers[2] = random.Next(10);
     }
-
-    private void StartGame()
+    private void UpdateLog(string message)
     {
-        answer = random.Next(1, intRandomMax + 1);
-        txtGuess.Enabled = true;
-        btnStart.Text = "Reset";
-        lblResults[5].Text = " range of 1 to " + intRandomMax.ToString();
-    }
-
-    private void GameWon()
-    {
-        txtGuess.Enabled = false;
-        lblResults[5].Text = " You Won!!!!";
-    }
-
-    private void GameLost()
-    {
-        txtGuess.Enabled = false;
-        lblResults[5].Text = " you Lost, Try again??";
+        lblResults[3].Text = lblResults[1].Text;
+        lblResults[3].Text = message;
     }
 
     private void btnStart_Click(object sender, EventArgs e)
     {
         if (btnStart.Text == "Start")
         {
-            StartGame();
+            ResetGame();
+            txtGuess1.Enabled = txtGuess2.Enabled = txtGuess3.Enabled = true;
+            btnStart.Text = "Reset";
+            btnCheat.Enabled = true;
         }
         else
         {
@@ -104,30 +86,121 @@ public partial class Form1 : Form
         }
     }
 
-    private void radioButton1_CheckedChanged(object sender, EventArgs e)
+
+    private void SubmitGuess()
     {
-        RadioButton rbEasy = (RadioButton)sender;
-        if (rbEasy.Checked)
+        int guess1, guess2, guess3;
+
+        if (int.TryParse(txtGuess1.Text, out guess1) &&
+            int.TryParse(txtGuess2.Text, out guess2) &&
+            int.TryParse(txtGuess3.Text, out guess3))
         {
-            intRandomMax = 10;
+            int correctCount = 0;
+            bool[] close = new bool[3];
+            for (int i = 0; i < 3; i++)
+            {
+                if (safeNumbers[i] - guess1 == 1 || safeNumbers[i] - guess1 == -1
+                    || safeNumbers[i] - guess2 == 1 || safeNumbers[i] - guess2 == -1
+                    || safeNumbers[i] - guess3 == 1 || safeNumbers[i] - guess3 == 1)
+                {
+                    close[i] = true;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (safeNumbers[i] == guess1 || safeNumbers[i] == guess2 || safeNumbers[i] == guess3)
+                {
+                    correctCount++;
+                    lblResults[i].Text = "Correct!";
+                    lblResults[i].ForeColor = Color.Green;
+                    pictureBoxCorrect.Visible = true;
+                    pictureBoxClose.Visible = false;
+                    pictureBoxFail.Visible = false;
+                }
+                else if (close[i])
+                {
+                    lblResults[i].Text = "Close";
+                    lblResults[i].ForeColor = Color.Blue;
+                    pictureBoxCorrect.Visible = false;
+                    pictureBoxClose.Visible = true;
+                    pictureBoxFail.Visible = false;
+                }
+                else
+                {
+                    lblResults[i].Text = "Not close";
+                    lblResults[i].ForeColor = Color.Red;
+                    pictureBoxCorrect.Visible = false;
+                    pictureBoxClose.Visible = false;
+                    pictureBoxFail.Visible = true;
+                }
+
+            }
+
+            string guessStatus = $"{txtGuess1.Text.PadLeft(2)} - {lblResults[0].Text} {txtGuess2.Text.PadLeft(2)} - {lblResults[1].Text} {txtGuess3.Text.PadLeft(2)} - {lblResults[2].Text}";
+            guessHistory.Insert(0, guessStatus); // Insert the guess status at the beginning of the list
+
+            // Clear existing controls from the TableLayoutPanel
+            tableLayoutPanel1.Controls.Clear();
+
+            // Add the guess history to the TableLayoutPanel
+            for (int i = 0; i < guessHistory.Count; i++)
+            {
+                Label lblGuessInfo = new Label
+                {
+                    Text = guessHistory[i],
+                    TextAlign = ContentAlignment.MiddleLeft,
+                    AutoSize = true
+                };
+                tableLayoutPanel1.Controls.Add(lblGuessInfo, 0, i);
+            }
+
+            numTurn++;
+            if (correctCount == 3)
+            {
+                UpdateLog("Congratulations! You Won! Play Again?");
+                txtGuess1.Enabled = txtGuess2.Enabled = txtGuess3.Enabled = false;
+                btnStart.Text = "Start";
+            }
+            else if (numTurn >= maxTurns)
+            {
+                UpdateLog("You're out of turns. Game over. Play Again?");
+                txtGuess1.Enabled = txtGuess2.Enabled = txtGuess3.Enabled = false;
+                btnStart.Text = "Start";
+            }
+            else
+            {
+                UpdateLog("Turn " + numTurn + " completed.");
+            }
+        }
+        else
+        {
+            MessageBox.Show("Please enter valid numbers in all three boxes.");
+        }
+    }
+    private void txtGuess_KeyPress(object sender, KeyPressEventArgs e)
+    {
+        if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+        {
+            SubmitGuess();
         }
     }
 
-    private void radioButton2_CheckedChanged(object sender, EventArgs e)
+    private void btnCheat_Click(object sender, EventArgs e)
     {
-        RadioButton rbMedium = (RadioButton)sender;
-        if (rbMedium.Checked)
-        {
-            intRandomMax = 20;
-        }
+        lblResults[0].Text = "Cheat: " + safeNumbers[0];
+        lblResults[1].Text = "Cheat: " + safeNumbers[1];
+        lblResults[2].Text = "Cheat: " + safeNumbers[2];
+        btnCheat.Enabled = false;
     }
 
-    private void radioButton3_CheckedChanged(object sender, EventArgs e)
+    private void btnGuess_Click(object sender, EventArgs e)
     {
-        RadioButton rbHard = (RadioButton)sender;
-        if (rbHard.Checked)
-        {
-            intRandomMax = 50;
-        }
+        SubmitGuess();
     }
+    private void btnExit_Click(object sender, EventArgs e)
+    {
+        Dispose();
+    }
+
+
 }
