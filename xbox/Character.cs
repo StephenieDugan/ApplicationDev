d5e362d846ab5ebfc42e0f8eecb09f653da399bd
StephenieDugan <stepheniedugan617@gmail.com>
2023-11-08 14:45:54 -0700

    save
    saving

---
+++ xbox/Character.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rise_of_the_Neu_Republic
+{
+    internal class Character
+    {
+    }
+}
---
+++ xbox/Utilities/StormTrooper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rise_of_the_Neu_Republic.Utilities
+{
+    internal class StormTrooper
+    {
+    }
+}
